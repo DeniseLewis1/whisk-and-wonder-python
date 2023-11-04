182bcf305fc5c7ea8ed55691601d76e3a5be52c4
Denise <deniselewis1120@gmail.com>
2023-11-03 17:17:24 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Create menu
+#   1. View all ideas
+#   2. View favorite ideas
+#   3. View completed ideas
+#   4. Search ideas
+#   5. Add ideas
+#   6. Edit ideas
+#   7. Delete ideas
+
+# Save list of ideas in json files
+#   All ideas, favorites, completed
+#   Use a dictionary to store ideas
+#     Key: idea ("")
+#     Value: list of tags ([])
+#   All ideas need to save the favorites and completed statuses
