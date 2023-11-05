278b5ba3ca609bd19fcf24ca056e85bb5daa8322
Denise <deniselewis1120@gmail.com>
2023-11-05 15:24:27 -0800

    Add placeholders for functions

--- IdeasOrganizer.py
+++ IdeasOrganizer.py
@@ -3,3 +3,24 @@ class IdeasOrganizer:
     self.all = all
     self.favorites = favorites
     self.completed = completed
+
+  def get_all(self):
+    return 
+
+  def get_favorites(self):
+    return
+
+  def get_completed(self):
+    return
+
+  def search(self, word):
+    return
+
+  def add_idea(self, idea, tags):
+    return
+
+  def edit_idea(self, idea):
+    return
+
+  def delete_idea(self, idea):
+    return
