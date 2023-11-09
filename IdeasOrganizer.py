e75a79264053037170d85ea7f25910a14bc92f6e
Denise <deniselewis1120@gmail.com>
2023-11-08 16:31:34 -0800

    Add a space after comma in split method

--- IdeasOrganizer.py
+++ IdeasOrganizer.py
@@ -19,16 +19,19 @@ class IdeasOrganizer:
     for idea in self.all:
       if word in idea:
         matches[idea] = self.all[idea]
+        print(f"1. {matches[idea]}")
       if word in self.all[idea]:
         matches[idea] = self.all[idea]
+        print(f"2. {matches[idea]}")
     return matches
 
   def add_idea(self, idea, tags):
-    self.all[idea] = list(tags.split(","))
+    self.all[idea] = list(tags.split(", "))
+    print(self.all[idea])
     return f"{idea} has been added"
 
   def edit_idea(self, idea, tags):
-    self.all[idea] = list(tags.split(","))
+    self.all[idea] = list(tags.split(", "))
     return f"{idea} has been updated"
 
   def delete_idea(self, idea):
